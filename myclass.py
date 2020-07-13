7be717a71b6e653a2aafbd2db320b39dadbff8b3
pkliui <44207525+pkliui@users.noreply.github.com>
2020-07-13 18:34:52 +0300

    Update myclass.py

--- myclass.py
+++ myclass.py
@@ -3,6 +3,10 @@ import numpy as np
 class MyClass:
     """A simple example class"""
     i = 12345
+    print(1+1)
+    print("This is a simple example class")
 
     def f(self):
         return 'hello world'
+    
+
